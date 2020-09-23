e15ff18f92dc475a09a5fdcff318e142d32b2c72
baekkwanghyoun <topking7180@gmail.com>
2020-09-22 23:09:55 -0700

    web작업 완료

--- app/Http/Controllers/StataController.php
+++ app/Http/Controllers/StataController.php
@@ -40,7 +40,7 @@ class StataController extends Controller
             fclose($fo);
 
             //$output = shell_exec("Stata.exe /q /e do C:/project/stata_web/public/stata16/${filename}.do");
-            $output = shell_exec("Stata.exe /q /e do C:/www/stata_web_git/public/stata16/${filename}.do");
+            $output = shell_exec("C:/stata/isstata/Stata.exe /q /e do C:/www/stata_web_git/public/stata16/${filename}.do");
 
 
             $fileread = file_get_contents(public_path()."\\"."${filename}.log", true);
