d66c4cc46e70a6bcb89a8858fe29502db628aa48
baekkwanghyoun <topking7180@gmail.com>
2020-12-03 23:17:20 -0800

    멀티통신 테스트중

--- app/Http/Controllers/StataController.php
+++ app/Http/Controllers/StataController.php
@@ -42,6 +42,7 @@ class StataController extends Controller
 
     public function store(Request $request)
     {
+        $sid = session()->getId();
 //        try {
             if (!empty($_POST['filename'])) {
 
@@ -110,69 +111,62 @@ class StataController extends Controller
 
 
 //        try {
-        $_POST['filename'] = 'testSession';
-        if (!empty($_POST['filename'])) {
-            if(env('APP_ENV')=='local') {
-                $text = "cd C:\project\stata_web\public\klips\n";
-            }
-            else {
-                $text = "cd C:\www\stata_web_git\public\klips\n";
-            }
+        //$_POST['filename'] = 'testSession';
+        if (env('APP_ENV') == 'local') {
+            $text = "cd C:\project\stata_web\public\klips\n";
+        } else {
+            $text = "cd C:\www\stata_web_git\public\klips\n";
+        }
 
 
-            $households = implode(" ", is_array(request('kt_select2_3'))?request('kt_select2_3'):array(request('kt_select2_3') ));
-            $persons = implode(" ", is_array(request('kt_select2_4'))?request('kt_select2_4'):array(request('kt_select2_4') ));
-            $waves = implode(" ", is_array(request('kt_select2_5'))?request('kt_select2_5'):array(request('kt_select2_5') ));
-            $text .= "smart_klips ${households} {$persons} , wave( {$waves}) wd( ) website( ) save( )";
+        $households = implode(" ", is_array(request('kt_select2_3')) ? request('kt_select2_3') : array(request('kt_select2_3')));
+        $persons = implode(" ", is_array(request('kt_select2_4')) ? request('kt_select2_4') : array(request('kt_select2_4')));
+        $waves = implode(" ", is_array(request('kt_select2_5')) ? request('kt_select2_5') : array(request('kt_select2_5')));
+        $text .= "smart_klips ${households} {$persons} , wave( {$waves}) wd( ) website( ) save( )";
 //            $text .= $households;
 //            $text .= $persons;
 //            $text .= $waves;
 
-            $filename = $_POST['filename'];
+        $filename = 'session' . session()->getId();//$_POST['filename'];
 
 
+        $fo = fopen('stata16/' . $filename . ".do", "w+");
+        fwrite($fo, $text);
+        fclose($fo);
 
-            $fo = fopen('stata16/'.$filename. ".do","w+");
-            fwrite($fo,$text);
-            fclose($fo);
-
-            if(env('APP_ENV')=='local')
-            {
-                $output = shell_exec("Stata.exe /q /e do C:/project/stata_web/public/stata16/${filename}.do");
-            }
-            else {
-                $output = shell_exec("C:/stata/isstata/Stata.exe /q /e do C:/www/stata_web_git/public/stata16/${filename}.do");
-            }
-
-            $fileread = file_get_contents(public_path()."\\"."${filename}.log", true);
-//            $fileread = htmlentities($fileread);
-            $fileread = preg_replace("/(\r\n\r\n)/i","<br />\n", $fileread);
-            $fileread = preg_replace("/  /i","&nbsp;&nbsp;", $fileread);
-            $fileread = preg_replace("/(<br\s*\/>)+/", "", $fileread);
-
-            $isSuccess = false;
-            if(Str::contains($fileread, 'saved')) {
-                $isSuccess = true;
-            }
-
-            //$fileread = preg_replace('/[\n\r]+/', '', $fileread);
-
-            //$fileread = Str::replaceFirst('&rt;br',"", $fileread);
-            //echo($fileread);
-            //session()->flashInput([$fileread]);
-            session()->flashInput($request->input());
-            //return view('stata.index', compact('fileread', 'isSuccess'));
-            Session::flash('isSuccess', true);
-            return back()->withInput();
-
-
-            //dump($fileread);
-            //VarDumper::dump($fileread);
-
-            //$output = shell_exec(\Storage::disk('public')->get("stata/exe.bat"));
-            //Stata.exe /q /e do stata16/b.do
-
+        if (env('APP_ENV') == 'local') {
+            $output = shell_exec("Stata.exe /q /e do C:/project/stata_web/public/stata16/${filename}.do");
+        } else {
+            $output = shell_exec("C:/stata/isstata/Stata.exe /q /e do C:/www/stata_web_git/public/stata16/${filename}.do");
         }
+
+        $fileread = file_get_contents(public_path() . "\\" . "${filename}.log", true);
+//            $fileread = htmlentities($fileread);
+        $fileread = preg_replace("/(\r\n\r\n)/i", "<br />\n", $fileread);
+        $fileread = preg_replace("/  /i", "&nbsp;&nbsp;", $fileread);
+        $fileread = preg_replace("/(<br\s*\/>)+/", "", $fileread);
+
+        $isSuccess = false;
+        if (Str::contains($fileread, 'saved')) {
+            $isSuccess = true;
+        }
+
+        //$fileread = preg_replace('/[\n\r]+/', '', $fileread);
+
+        //$fileread = Str::replaceFirst('&rt;br',"", $fileread);
+        //echo($fileread);
+        //session()->flashInput([$fileread]);
+        session()->flashInput($request->input());
+        //return view('stata.index', compact('fileread', 'isSuccess'));
+        Session::flash('isSuccess', true);
+        return back()->withInput();
+
+
+        //dump($fileread);
+        //VarDumper::dump($fileread);
+
+        //$output = shell_exec(\Storage::disk('public')->get("stata/exe.bat"));
+        //Stata.exe /q /e do stata16/b.do
         /* }catch(\Throwable $e){
              Log::debug($e);
          }*/
