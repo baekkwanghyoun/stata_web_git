f7161e0e30f46e35de165e80e90a0b1823dd5e51
baekkwanghyoun <topking7180@gmail.com>
2021-12-30 13:01:51 -0800

    cors활성화

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://smartklips.kli.re.kr'],
 
     'allowed_origins_patterns' => [],
 
