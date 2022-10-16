f56e1eff8b01e7497c64002bf67979957891b4e5
sean_win11_sail <silverjava@naver.com>
2022-10-16 15:03:38 -0700

    mac test

---
+++ app/Http/Controllers/BlakeGardner/MacAddress.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace BlakeGardner;
+
+/**
+ * This class allows you to preform various operations with
+ * Media Access Control (MAC addresses) on UNIX type systems.
+ *
+ * @author Blake Gardner <blakegardner[at]cox.net>
+ * @copyright Copyright (c) 2012, Blake Gardner
+ * @license MIT License (see License.txt)
+ */
+class MacAddress
+{
+
+
+}
