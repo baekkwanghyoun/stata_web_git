c25a358020c9cda7d79616e983d0126828c2aaa8
sean_win11_sail <silverjava@naver.com>
2022-11-01 23:26:56 -0700

    api 연동

--- app/Http/Controllers/StataController.php
+++ app/Http/Controllers/StataController.php
@@ -675,7 +675,8 @@ dump($macAddr);
         else if($tab==='search') {
             //$fileread = Storage::get('stata16/log/'.$nowDate.'/'.$foldername.'/'.$filename_req.'.log');
             //$fileread = fopen(Storage::path("/storage/stata16/log/${nowDate}/${foldername}/${filename_req}.log"),'r');
-            $fileread = fopen(Storage::path("/storage/stata16/log/${nowDate}/${foldername}/filename_req.log"),'r');
+
+            $fileread = fopen(Storage::path("/storage/stata16/log/${nowDate}/${foldername}/klips_final.log"),'r');
            return response()->json($fileread);
         }
 
